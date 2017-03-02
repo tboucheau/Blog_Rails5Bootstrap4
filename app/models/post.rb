9a4732ae1e8afe9ac95f6301c732c80cd7ff1f62
frogtube <u.pradere@gmail.com>
2017-03-02 10:46:23 +0100

    Add navbar, routes to posts, bootstrap grid, scss

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,12 @@
 class Post < ApplicationRecord
 
 	extend FriendlyId
-  	friendly_id :title, use: :slugged
+  friendly_id :title, use: :slugged
+	
+	scope :most_recent, -> { order(id: :desc) }
 
-  	def should_generate_new_friendly_id?
-  		title_changed?
-  	end
+  def should_generate_new_friendly_id?
+  	title_changed?
+  end
 
 end
