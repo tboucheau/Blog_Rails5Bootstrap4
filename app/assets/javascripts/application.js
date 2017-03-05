b364c151f5792d88e9db5d3f2d49254271b93f38
frogtube <u.pradere@gmail.com>
2017-03-05 11:44:00 +0100

    Add tinyMCE for post publishing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,12 @@
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+document.addEventListener("turbolinks:load", function() {
+  tinymce.remove();
+  tinymce.init({ 
+    selector:'textarea#post_body',
+    plugins: "codesample image media link code",
+    toolbar: "undo redo | styleselect | bold italic link | codesample image media | code"
+  });
+})
